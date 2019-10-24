6efaf40be9fab002f90108b41f4426dbeb7c5651
danielkaylor <danielkaylor@DESKTOP-DQ6MO5B.localdomain>
2019-10-23 20:46:28 -0400

    added wireless

---
+++ wireless.py
@@ -0,0 +1,23 @@
+import socket
+import RPi.GPIO as GPIO
+import time
+
+UDP_IP = "192.168.1.2"
+UDP_PORT = 57222
+
+sock = socket.socket(socket.AF_INET, # Internet
+                    socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18, GPIO.OUT)
+
+while True:
+    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    print("received message: {}".format(data))
+
+    if int(data) == 0:
+		GPIO.output(18, GPIO.HIGH)
+	else:
+		GPIO.output(18, GPIO.LOW)
