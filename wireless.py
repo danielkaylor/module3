ef0c148da2ed326cdd8aa4c7c7dfad6f4434e074
danielkaylor <danielkaylor@DESKTOP-DQ6MO5B.localdomain>
2019-10-23 23:23:54 -0400

    actually fixed wireless

--- wireless.py
+++ wireless.py
@@ -13,7 +13,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18, GPIO.OUT)
 GPIO.setup(16, GPIO.OUT)
-GPIO.setup(13, GPIO.OUT)
+GPIO.setup(23, GPIO.OUT)
 GPIO.setup(21, GPIO.OUT)
 
 while True:
@@ -23,10 +23,10 @@ while True:
 	if int(data) == 0:
 		GPIO.output(18, GPIO.HIGH)
 		GPIO.output(16, GPIO.HIGH)
-		GPIO.output(13, GPIO.HIGH)
+		GPIO.output(23, GPIO.HIGH)
 		GPIO.output(21, GPIO.HIGH)
 	else:
 		GPIO.output(18, GPIO.LOW)
 		GPIO.output(16, GPIO.LOW)
-		GPIO.output(13, GPIO.LOW)
+		GPIO.output(23, GPIO.LOW)
 		GPIO.output(21, GPIO.LOW)
