6ec0b046eeb1b50c562c3bbbdde99bfe9de37086
danielkaylor <danielkaylor@DESKTOP-DQ6MO5B.localdomain>
2019-10-23 22:56:56 -0400

    added lights

--- wireless.py
+++ wireless.py
@@ -12,6 +12,9 @@ sock.bind((UDP_IP, UDP_PORT))
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(21, GPIO.OUT)
 
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
@@ -19,5 +22,11 @@ while True:
 
     if int(data) == 0:
 		GPIO.output(18, GPIO.HIGH)
+		GPIO.output(16, GPIO.HIGH)
+		GPIO.output(13, GPIO.HIGH)
+		GPIO.output(21, GPIO.HIGH)
 	else:
 		GPIO.output(18, GPIO.LOW)
+		GPIO.output(16, GPIO.LOW)
+		GPIO.output(13, GPIO.LOW)
+		GPIO.output(21, GPIO.LOW)
